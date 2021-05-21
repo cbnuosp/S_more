08608cd0f405b4fa8df74f722a198285637feec1
baeleekang <50096266+baeleekang@users.noreply.github.com>
2021-05-21 11:31:25 +0900

    LoginPisMain

--- app/src/main/java/com/example/android_tf1/LoginActivity.java
+++ app/src/main/java/com/example/android_tf1/LoginActivity.java
@@ -3,24 +3,37 @@ package com.example.android_tf1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//타이틀바 제거
         setContentView(R.layout.activity_login);
 
         TextView registerButton = (TextView)findViewById(R.id.registerButton);
        registerButton.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+               Intent registerIntent = new Intent(getApplicationContext(), RegisterActivity.class);
                LoginActivity.this.startActivity(registerIntent);
            }
        });
+
+        Button loginB = (Button)findViewById(R.id.login);
+        loginB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+                LoginActivity.this.startActivity(mainIntent);
+            }
+        });
     }
 }
